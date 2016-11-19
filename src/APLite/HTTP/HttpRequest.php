2994d355548974ae195647149b790f65e7fe842a
designinlife <wuxuexuan@msn.com>
2016-11-19 13:17:37 +0800

    fix.

--- src/APLite/HTTP/HttpRequest.php
+++ src/APLite/HTTP/HttpRequest.php
@@ -1,7 +1,7 @@
 <?php
 namespace APLite\HTTP;
 
-use APLite\Interfaces\IBaseArray;
+use APLite\Base\DataSerializable;
 
 /**
  * HTTP Request 类。
@@ -11,7 +11,7 @@ use APLite\Interfaces\IBaseArray;
  * @version       1.0.0
  * @copyright (c) 2013-2016, Lei Lee
  */
-abstract class HttpRequest implements IBaseArray {
+abstract class HttpRequest extends DataSerializable {
     /**
      * 连接超时。(毫秒)
      *
@@ -167,47 +167,5 @@ abstract class HttpRequest implements IBaseArray {
      */
     function getUrl() {
         return $this->url;
-    }
-
-    /**
-     * 对象转换为数组。
-     *
-     * @param array $options
-     * @return array
-     */
-    function toArray(array $options = []) {
-        $d     = [];
-        $ref   = new \ReflectionClass($this);
-        $props = $ref->getProperties(\ReflectionProperty::IS_PUBLIC | \ReflectionProperty::IS_PROTECTED);
-
-        foreach ($props as $prop) {
-            $prop->setAccessible(true);
-
-            $d[$prop->getName()] = $prop->getValue($this);
-        }
-
-        return $d;
-    }
-
-    /**
-     * 对象转换为 JSON 字符串。
-     *
-     * @param array $options
-     * @return string
-     */
-    function toJSONString(array $options = []) {
-        $d = $this->toArray($options);
-
-        return json_encode($d, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-    }
-
-    /**
-     * The __toString method allows a class to decide how it will react when it is converted to a string.
-     *
-     * @return string
-     * @link http://php.net/manual/en/language.oop5.magic.php#language.oop5.magic.tostring
-     */
-    function __toString() {
-        return \msgpack_serialize($this);
     }
 }
